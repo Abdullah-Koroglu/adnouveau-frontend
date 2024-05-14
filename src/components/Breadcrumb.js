9f41807c611279cd91e86f4f1b5972a8c5d7fb38
Abdullah Koroglu <abdullah34koroglu@gmail.com>
2024-05-14 12:03:23 +0300

    animation not loading bug fixed

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -5,6 +5,7 @@ import { capitalize } from '@/components/helper/index';
 import { useBreadcrumb } from '@/app/Providers';
 import { useEffect } from 'react';
 import { animatePageIn } from './animation/animations';
+import { FaHashtag } from "react-icons/fa6";
 
 
 const Breadcrumb = () => {
@@ -20,10 +21,10 @@ const Breadcrumb = () => {
     const pathSegments = paths.filter(path => path !== '')
 
     return (
-      <nav 
-        aria-label="Breadcrumb" 
-        className="breadcrumb bg-white dark:bg-black max-w-[100vw] transition-all duration-300 overflow-hidden py-2 px-4 md:px-8 lg:px-16 ml-auto mr-auto 3xl:w-2/3 rounded-2xl mt-4 md:mt-10"
-        style={{display: 'flex'}}
+      <nav
+        aria-label="Breadcrumb"
+        className="breadcrumb max-w-[100vw] transition-all duration-300 overflow-hidden py-2 px-4 md:px-8 lg:px-16 ml-auto mr-auto 3xl:w-2/3 rounded-2xl mt-4 md:mt-10"
+        style={{ display: 'flex' }}
       >
         {
           pathSegments.length > 0 ?
@@ -45,13 +46,14 @@ const Breadcrumb = () => {
                     {index !== 0 ? <span className="mx-1">/</span> : null}
                     <Link className={`${index + 1 === pathSegments.length ? 'text-black dark:text-white' : ''} text-nowrap transition-all duration-150`} href={`/${pathSegments.slice(0, index + 1).join('/')}`}>
                       {(breadcrumb?.title && index + 1 === pathSegments.length && segmentText.match(/^\d+$/gm)) ? breadcrumb?.title : capitalize(segmentText)}
-                      {index === 0 ? 's': ''}
+                      {index === 0 ? 's' : ''}
                     </Link>
                   </li>
                 )
               })}
             </ol> :
             null}
+        <FaHashtag className="opacity-0" />
       </nav>
     );
   }
